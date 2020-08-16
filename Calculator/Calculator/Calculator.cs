5201fba4abbd3cad7581023a4a686759ce0a2032
Morten Kristensen <Mortenkristensen13@gmail.com>
2020-08-16 13:35:39 +0200

    Initial commit.

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator {
+
+    /// <summary>
+    /// Defines a simple calculator.
+    /// </summary>
+    public class Calculator {
+
+        /// <summary>
+        /// Adds two values.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b) => a + b;
+    }
+}
